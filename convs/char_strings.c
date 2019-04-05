bad1680e4bb4d55d1bd7d891d4b18a5782387c4d
Mathieu JOUFFROY <mjouffro@e3r7p11.42.fr>
2019-04-05 16:25:05 +0200

    refactoring

--- convs/char_strings.c
+++ convs/char_strings.c
@@ -6,7 +6,7 @@
 /*   By: mjouffro <mjouffro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/05 15:41:28 by mjouffro          #+#    #+#             */
-/*   Updated: 2019/04/05 15:42:04 by mjouffro         ###   ########.fr       */
+/*   Updated: 2019/04/05 16:25:01 by mjouffro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,44 +64,3 @@ void	wstr_conv(t_printf *pf)
 {
 
 }
-
-int		s_conv(t_printf *pf, char *str)
-{
-	int		len;
-	int		min;
-
-	len = str ? ft_strlen(str) : 6;
-	if (pf->precision && pf->precision < len)
-		len = (pf->precision == -1) ? 0 : pf->precision;
-	min = pf->min_length ? pf->min_length - len : 0;
-	if (pf->flags & F_MINUS)
-	{
-		str ? handle_buff(pf, str, 0, len) : handle_buff(pf, "(null)", 0, len);
-		min_padding(pf, ' ', min);
-	}
-	else
-	{
-		(pf->flags & F_ZERO) ? min_padding(pf, '0', min) :
-		min_padding(pf, ' ', min);
-		str ? handle_buff(pf, str, 0, len) : handle_buff(pf, "(null)", 0, len);
-	}
-	return (1);
-}
-
-int		s_arg(t_printf *pf)
-{
-	char	*str;
-	wchar_t *wstr;
-
-	if ((pf->flags & S_LONG) || (pf->conv == 'S'))
-	{
-		wstr = va_arg(pf->ap, wchar_t*);
-		ws_conv(pf, wstr);
-	}
-	else
-	{
-		str = va_arg(pf->ap, char*);
-		s_conv(pf, str);
-	}
-	return (1);
-}*/
